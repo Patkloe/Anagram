d2a279e6f535c345805dddedfbc4cabef3e9c4a7
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-19 09:52:56 -0600

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -15,3 +15,24 @@ else
    }
 first;
 difference;
+// another version
+var s = "anagram";
+var t = "nagarak";
+var first = s.split("");
+var second =  t.split("");
+//var difference = first.filter(x => second.indexOf(x) === -1);
+//difference;
+//var size = difference.length;
+var s1 = first.sort().join("");
+var s2 = second.sort().join("");
+
+if( s1 === s2)
+   {
+    alert ("anagram");
+   }
+else
+   {
+    alert ("not anagram");
+   }
+//first;
+//difference;
