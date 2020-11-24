961ec1b5d13c8688d2863ca0291199074979e129
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-23 20:56:06 -0600

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,3 +1,4 @@
+// version 1
 var s = "anagram";
 var t = "nagarak";
 var first = s.split("");
@@ -36,3 +37,44 @@ else
    }
 //first;
 //difference;
+// algorithm version
+function anagram (a1, a2) {
+ if(a1.length !== a2.length){
+      alert("not anagram");
+      return false;
+  }
+  else {
+
+    var a = {}, diff = [];
+
+    for (var i = 0; i < a1.length; i++) {
+        a[a1[i]] = true;
+    }
+    //console.log("a1");
+    //for(var j in a){
+     //  alert(a[j]);
+    //}
+    for (var i = 0; i < a2.length; i++) {
+        if (a[a2[i]]) {
+            delete a[a2[i]];
+        } else {
+            a[a2[i]] = true;
+        }
+    }
+    for (var k in a) {
+        diff.push(k);
+    }
+  if(diff.length === 0){
+     alert("anagram");
+     return true;
+   }
+   else{
+     alert("not anagram");
+     return false;
+   }
+  }
+}
+
+//console.log(anagram(['a', 'b'], ['b', 'a']));
+//console.log(anagram("abcd", "abcde"));
+console.log(anagram("bxc", "xcb"));
